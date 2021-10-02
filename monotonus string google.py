d73c9ac5578a973f4b1d22f5e46e3bf43dc2a071
physicsgtx <60617282+physicsgtx@users.noreply.github.com>
2021-10-02 15:56:24 +0530

    updated  monotonus string google.py
    Added more details to  (monotonus string google.py) file and   Time Complexity: O(N) 
    Auxiliary Space: O(1)

--- monotonus string google.py
+++ monotonus string google.py
@@ -1,25 +1,77 @@
-s = input()
-n= len(s)-1
-sol = 0
-idx=0
-while(idx!=n):
-    
-    sol+=1
-    
-    inc = idx
-    dec = idx
-
-    while(inc + 1<n and s[inc]==s[inc+1]):
-        inc+=1
-    while(dec+1<n and s[dec]==s[dec+1]):
-        dec+=1
-
-    if (max(inc,dec)+1==n):
-        break;
-
-    idx = max(inc,dec)+1
-    print(inc)
-
-
-print(sol)    
-    
+# Function to return final result
+def minReqSubstring(s, n):
+     
+    # Initialize variable to keep
+    # track of ongoing sequence
+    ongoing = 'N'
+    count, l = 0, len(s)
+     
+    for i in range(1, l):
+         
+        # If current sequence is neither
+        # increasing nor decreasing
+        if ongoing == 'N':
+             
+            # If prev char is greater
+            if s[i] < s[i - 1]:
+                ongoing = 'D'
+                 
+            # If prev char is same
+            elif s[i] == s[i - 1]:
+                ongoing = 'N'
+                 
+            # Otherwise
+            else:
+                ongoing = 'I'
+                 
+        # If current sequence
+        # is Non-Decreasing
+        elif ongoing == 'I':
+             
+            # If prev char is smaller
+            if s[i] > s[i - 1]:
+                ongoing = 'I'
+                 
+            # If prev char is same
+            elif s[i] == s[i - 1]:
+                 
+                # Update ongoing
+                ongoing = 'I'
+             
+            # Otherwise
+            else:
+                 
+                # Update ongoing
+                ongoing = 'N'
+                 
+                # Increase count
+                count += 1
+             
+        # If current sequence
+        # is Non-Increasing
+        else:
+             
+            # If prev char is greater,
+            # then update ongoing with D
+            if s[i] < s[i - 1]:
+                ongoing = 'D'
+                 
+            # If prev char is equal, then
+            # update current with D
+            elif s[i] == s[i - 1]:
+                ongoing = 'D'
+                 
+            # Otherwise, update ongoing
+            # with N and increment count
+            else:
+                ongoing = 'N'
+                count += 1
+                 
+    # Return count + 1
+    return count + 1
+ 
+# Driver code
+S = "aeccdhba"
+n = len(S)
+ 
+print(minReqSubstring(S, n))
